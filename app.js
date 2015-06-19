ebd399998df14cd58d248a874123ef752065cccf
romaintailhurat <romain.tailhurat@gmail.com>
2015-06-19 15:41:20 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+const greetings = 'Pogues mock server, see doc at https://github.com/InseeFr/Pogues/blob/master/doc/pogues-swagger.yaml';
+
+server.connection({ port: 3000 });
+
+let run = () => console.log('Server running at:', server.info.uri);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => reply(greetings)
+});
+
+server.route({
+  method: 'GET',
+  path: '/pogues/questionnaires',
+  handler: (request, reply) => reply.file('./pogues-questionnaires-list.json')
+});
+
+server.route({
+  method: 'GET',
+  path: '/pogues/questionnaire/{id}',
+  handler: (request, reply) => reply.file('./pogues-sample-questionnaire.json')
+});
+
+server.start(run);
---
+++ index.js
@@ -0,0 +1,5 @@
+// Babel hook for activating ES6 transpiling
+// See http://babeljs.io/docs/usage/require/
+require('babel/register');
+
+module.exports = require('./app.js');
