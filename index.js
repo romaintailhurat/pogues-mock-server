31688087441fa93558fd76af4dffb71ee1d523c9
romaintailhurat <romain.tailhurat@gmail.com>
2015-12-23 14:33:58 +0100

    Adding a home page and refactoring the API routes

--- index.js
+++ index.js
@@ -4,28 +4,42 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 
-const greetings = 'Pogues mock server, see doc at https://github.com/InseeFr/Pogues/blob/master/doc/pogues-swagger.yaml';
+const PATHS = {
+  home: '/',
+  pogues: {
+    questionnaires: '/pogues/questionnaires',
+    questionnaire: '/pogues/questionnaire/{id}'
+  },
+  stromae: '/stromae/publisher'
+};
 
 server.connection({ port: 3000 });
 
 let run = () => console.log('Server running at:', server.info.uri);
 
+/* Routes */
 server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, reply) => reply(greetings)
+    path: PATHS.home,
+    handler: (request, reply) => reply.file('home.html')
 });
 
 server.route({
   method: 'GET',
-  path: '/pogues/questionnaires',
+  path: PATHS.pogues.questionnaires,
   handler: (request, reply) => reply.file('./pogues-questionnaires-list.json')
 });
 
 server.route({
   method: 'GET',
-  path: '/pogues/questionnaire/{id}',
+  path: PATHS.pogues.questionnaire,
   handler: (request, reply) => reply.file('./pogues-sample-questionnaire.json')
+});
+
+server.route({
+  method: 'POST',
+  path: PATHS.stromae,
+  handler: (request, reply) => reply('yo') // TODO implement
 });
 
 server.start(run);
