71070d452d9d7d2e4ea3680b1b75113d9bb56bc3
romaintailhurat <romain.tailhurat@gmail.com>
2016-01-04 10:23:13 +0100

    Adding POST for creating questionnaire

--- index.js
+++ index.js
@@ -17,18 +17,35 @@ server.connection({ port: 3000 });
 
 let run = () => console.log('Server running at:', server.info.uri);
 
-/* Routes */
+/*
+Routes
+ */
+
+/* / */
 server.route({
     method: 'GET',
     path: PATHS.home,
     handler: (request, reply) => reply.file('home.html')
 });
 
+/* Persistance */
 server.route({
   method: 'GET',
   path: PATHS.pogues.questionnaires,
   handler: (request, reply) => reply.file('./pogues-questionnaires-list.json')
 });
+
+// TODO implement
+server.route({
+  method: 'POST',
+  path: PATHS.pogues.questionnaires,
+  handler: (request, reply) => {
+    let response = reply('new questionnaire created');
+    response.statusCode(201);
+    response.header('Location', server.info.uri)
+    response.header('Slug', 123456);
+  }
+})
 
 server.route({
   method: 'GET',
